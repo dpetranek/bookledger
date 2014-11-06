814b891b362094caed0bc37828bcf6312f13311d
dpetranek <danielpetranek@gmail.com>
2014-11-06 00:11:18 -0600

    implemented basic holy-grail layout

--- src/bookledger/views/layout.clj
+++ src/bookledger/views/layout.clj
@@ -10,15 +10,24 @@
     [:head
      [:title "Bookledger"]
      (include-css "/css/screen.css")]
-    [:body content]))
+    [:body
+     [:div {:id "container"}
+      [:div.header
+       [:h1 "Bookledger"]]
+      [:div.main
+       [:div.column {:id "left"}]
+       [:div.column {:id "center"} content]
+       [:div.column {:id "right"}
+        (if-let [user (session/get :user)]
+          [:div (link-to "/logout" (str "Logout " user))]
+          [:div (link-to "/register" "Register")
+           (form-to [:post "/login"]
+                    (text-field {:placeholder "Username"} "username")
+                    (password-field {:placeholder "Password"} "pass")
+                    (submit-button "Log In"))])]]
+      [:div.footer]]]))
+
+
 
 (defn common [& content]
-  (base
-   (if-let [user (session/get :user)]
-     [:div (link-to "/logout" (str "Logout " user))]
-     [:div (link-to "/register" "Register")
-      (form-to [:post "/login"]
-               (text-field {:placeholder "Username"} "username")
-               (password-field {:placeholder "Password"} "pass")
-               (submit-button "Log In"))])
-   content))
+  (base content))
