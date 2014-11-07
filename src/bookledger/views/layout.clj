20ff1fbbf47c8b2ac52e7ab694916b935c7152d3
dpetranek <danielpetranek@gmail.com>
2014-11-06 22:45:09 -0600

    fixed layout for consistency, grail in equilibrium

--- src/bookledger/views/layout.clj
+++ src/bookledger/views/layout.clj
@@ -10,24 +10,24 @@
     [:head
      [:title "Bookledger"]
      (include-css "/css/screen.css")]
-    [:body
-     [:div {:id "container"}
-      [:div.header
-       [:h1 "Bookledger"]]
+    [:body content]))
+
+(defn login-widget []
+  (if-let [user (session/get :user)]
+    [:div (link-to "/logout" (str "Logout " user))]
+    [:div (link-to "/register" "Register")
+     (form-to [:post "/login"]
+              (text-field {:placeholder "Username"} "username")
+              (password-field {:placeholder "Password"} "pass")
+              (submit-button "Log In"))]))
+
+(defn common [& content]
+  (base
+   [:div {:id "container"}
       [:div.main
        [:div.column {:id "left"}]
        [:div.column {:id "center"} content]
-       [:div.column {:id "right"}
-        (if-let [user (session/get :user)]
-          [:div (link-to "/logout" (str "Logout " user))]
-          [:div (link-to "/register" "Register")
-           (form-to [:post "/login"]
-                    (text-field {:placeholder "Username"} "username")
-                    (password-field {:placeholder "Password"} "pass")
-                    (submit-button "Log In"))])]]
-      [:div.footer]]]))
+       [:div.column {:id "right"} (login-widget)]]
+      [:div.footer]]))
 
 
-
-(defn common [& content]
-  (base content))
